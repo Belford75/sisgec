17576247e88bb660ccb9b3dfb6d1e383b74a1e1c
Aldair Guzman <aldair.guzman@gisul.com.bo>
2022-01-18 09:20:30 -0400

    Sweetalert y correccion de login

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
- if($_POST){
-    echo $_POST['usuario'];
- }
+
 
 ?>
 
@@ -15,22 +13,15 @@
     <meta http-equiv="Content-Language" content="en">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>TechnoChurch - Sistema informático</title>
-    <meta name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" />
     <meta name="description" content="This is an example dashboard created using build-in elements and components.">
     <meta name="msapplication-tap-highlight" content="no">
-    <!--
-    =========================================================
-    * ArchitectUI HTML Theme Dashboard - v1.0.0
-    =========================================================
-    * Product Page: https://dashboardpack.com
-    * Copyright 2019 DashboardPack (https://dashboardpack.com)
-    * Licensed under MIT (https://github.com/DashboardPack/architectui-html-theme-free/blob/master/LICENSE)
-    =========================================================
-    * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-    -->
+
     <link href="./main.css" rel="stylesheet">
     <link rel="icon" href="assets/images/logoB-Tch.ico">
+
+    <script src="assets/sweetalert/sweetalert2.min.js"></script>
+    <link rel="stylesheet" href="assets/sweetalert/sweetalert2.min.css">
 </head>
 
 <body>
@@ -65,8 +56,9 @@
                         </span>
                     </button>
                 </span>
-                
-            </div>    <div class="app-header__content">
+
+            </div>
+            <div class="app-header__content">
                 <div class="app-header-left">
                     <ul class="header-menu nav">
                         <li class="nav-item">
@@ -87,7 +79,7 @@
                                 Cambio de Usuario
                             </a>
                         </li>
-                    </ul>        
+                    </ul>
                 </div>
                 <div class="app-header-right">
                     <div class="header-btn-lg pr-0">
@@ -102,20 +94,19 @@
                                 </div>
                                 <div class="widget-content-left  ml-3 header-user-info">
                                     <div class="widget-heading">
-                                        <?php 
-                                            if($_POST){
-                                                echo $_POST['usuario']; 
-                                            } else {
-                                                echo ('USUARIO NO LOGUEADO');
-                                                
-                                            }
+                                        <?php
+                                        if ($_POST) {
+                                            echo $_POST['usuario'];
+                                        } else {
+                                            echo ('USUARIO NO LOGUEADO');
+                                        }
                                         ?>
                                     </div>
-                                    
+
                                 </div>
                             </div>
                         </div>
-                    </div>        
+                    </div>
                 </div>
             </div>
         </div>
@@ -125,8 +116,7 @@
                     <div class="logo-src"></div>
                     <div class="header__pane ml-auto">
                         <div>
-                            <button type="button" class="hamburger close-sidebar-btn hamburger--elastic"
-                                data-class="closed-sidebar">
+                            <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
                                 <span class="hamburger-box">
                                     <span class="hamburger-inner"></span>
                                 </span>
@@ -145,8 +135,7 @@
                 </div>
                 <div class="app-header__menu">
                     <span>
-                        <button type="button"
-                            class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
+                        <button type="button" class="btn-icon btn-icon-only btn btn-primary btn-sm mobile-toggle-header-nav">
                             <span class="btn-icon-wrapper">
                                 <i class="fa fa-ellipsis-v fa-w-6"></i>
                             </span>
@@ -158,7 +147,8 @@
                         <ul class="vertical-nav-menu">
                             <li class="app-sidebar__heading">FORMULARIOS</li> <!-- Inicio de los menús (lateral)-->
                             <li>
-                                <a href="#"> <!--Menú de personas-->
+                                <a href="#">
+                                    <!--Menú de personas-->
                                     <i class="metismenu-icon pe-7s-note2"></i>
                                     Administrativos
                                     <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
@@ -179,7 +169,8 @@
                                 </ul>
                             </li>
                             <li>
-                                <a href="#"> <!--Menú de finanzas-->
+                                <a href="#">
+                                    <!--Menú de finanzas-->
                                     <i class="metismenu-icon pe-7s-cash"></i>
                                     Finanzas
                                     <i class="metismenu-state-icon pe-7s-angle-down caret-left"></i>
@@ -221,7 +212,7 @@
                         <div class="page-title-wrapper">
                             <div class="page-title-heading">
                                 <div class="page-title-icon">
-                                   <img src="assets/images/logo-Tch.png">  
+                                    <img src="assets/images/logo-Tch.png">
                                 </div>
                                 <div>TechnoChurch
                                     <div class="page-title-subheading">Tecnología al servicio de la Iglesia.
@@ -231,20 +222,14 @@
                         </div>
                     </div>
                     <div class="ventana_usr header-shadow bg-night-sky header-text-light" id="direcDB">URL DE LA BASE DE DATOS
-                        <form class="form_usr"><br/>
-                            <div class="position-relative form-group"><label 
-                                    class="">Nombre</label><input name="nameDB" id="nombreDB"
-                                    placeholder="Nombre de la base de datos" type="text"
-                                    class="form-control"></div>
-                            <div class="position-relative form-group"><label 
-                                    class="">Dirección</label><input name="dirUrl"
-                                    id="direccionURl" placeholder="Dirección de la URL"
-                                    type="url" class="form-control"></div>
+                        <form class="form_usr"><br />
+                            <div class="position-relative form-group"><label class="">Nombre</label><input name="nameDB" id="nombreDB" placeholder="Nombre de la base de datos" type="text" class="form-control"></div>
+                            <div class="position-relative form-group"><label class="">Dirección</label><input name="dirUrl" id="direccionURl" placeholder="Dirección de la URL" type="url" class="form-control"></div>
                             <button class="mt-1 btn btn-primary">Crear</button>
                         </form>
                     </div>
                     <div class="ventana_usr header-shadow bg-night-sky header-text-light" id="ElegirDB">SELECCIONAR LA BASE DE DATOS
-                        <form class="form_usr"><br/>
+                        <form class="form_usr"><br />
                             <div>
                                 <ul class="list-group">
                                     <button class="list-group-item-action list-group-item">CENTRAL</button>
@@ -255,15 +240,9 @@
                         </form>
                     </div>
                     <div class="ventana_usr header-shadow bg-night-sky header-text-light" id="ventana_usuario">AUTENTIFICACION DE USUARIO
-                        <form class="form_usr" action="validar_usr.php" method="post"><br/>
-                            <div class="position-relative form-group"><label 
-                                    class="">Usuario</label><input name="usuario" id="idUsr"
-                                    placeholder="Nombre de usuario" type="text"
-                                    class="form-control"></div>
-                            <div class="position-relative form-group"><label for="examplePassword"
-                                    class="">Contraseña</label><input name="contraseña"
-                                    id="idPass" placeholder="Contraseña"
-                                    type="password" class="form-control"></div>
+                        <form class="form_usr" action="validar_usr.php" method="post"><br />
+                            <div class="position-relative form-group"><label class="">Usuario</label><input name="usuario" id="idUsr" placeholder="Nombre de usuario" type="text" class="form-control"></div>
+                            <div class="position-relative form-group"><label for="examplePassword" class="">Contraseña</label><input name="contraseña" id="idPass" placeholder="Contraseña" type="password" class="form-control"></div>
                             <button class="mt-1 btn btn-primary">Autenticar</button>
                         </form>
                     </div>
@@ -273,18 +252,23 @@
     </div>
     <script type="text/javascript" src="./assets/scripts/main.js"></script>
     <script>
-        function urlDB(){
-            document.getElementById("direcDB").style.display="block";
+        function MensajeErrorInicio() {
+            Swal.fire({
+                icon: 'error',
+                title: 'Usuario incorrecto'
+            });
         }
-    </script>
-    <script>
-        function ElegirDB(){
-            document.getElementById("ElegirDB").style.display="block";
+
+        function urlDB() {
+            document.getElementById("direcDB").style.display = "block";
         }
-    </script>
-    <script>
-        function abrirUsr(){
-            document.getElementById("ventana_usuario").style.display="block";
+
+        function ElegirDB() {
+            document.getElementById("ElegirDB").style.display = "block";
+        }
+
+        function abrirUsr() {
+            document.getElementById("ventana_usuario").style.display = "block";
         }
     </script>
 </body>
--- validar_usr.php
+++ validar_usr.php
@@ -1,27 +1,28 @@
 <?php
 
-$usuario=$_POST['usuario'];
-$contraseña=$_POST['contraseña'];
+$usuario = $_POST['usuario'];
+$contraseña = $_POST['contraseña'];
 session_start();
-$_SESSION['usuario']=$usuario;
 
-$conexion=mysqli_connect("localhost","root","","sisgec");
+$_SESSION['usuario'] = $usuario;
+$conexion = mysqli_connect("localhost", "root", "", "sisgec");
 
-$consulta="SELECT * FROM usuarios WHERE usuario='$usuario' and contraseña='$contraseña'";
-$resultado=mysqli_query($conexion,$consulta);
+$consulta = "SELECT * FROM usuarios WHERE usuario='$usuario' and contraseña='$contraseña'";
+$resultado = mysqli_query($conexion, $consulta);
 
 //$filas=mysqli_fetch_array($resultado);
-$filas=mysqli_fetch_row($resultado);
+$filas = mysqli_fetch_row($resultado);
 
-if($filas){
-  header("location:index.php");
-}else{
-  ?>
-  <?php
+if ($filas) {
   include("index.php");
-  ?>
-  
-  <?php
+  header("location:index.php");
+} else {
+
+  $_POST['usuario'] = 'USUARIO NO LOGUEADO';
+  include("index.php");
+  echo '<script language="javascript">';
+  echo 'MensajeErrorInicio();';
+  echo '</script>';
 }
 mysqli_free_result($resultado);
 mysqli_close($conexion);
